6dc727d8999b49f51f970eaae053cb99463fb8a9
jurinta kancleryte <jurinta.kancleryte@gmail.com>
2026-09-17 21:28:08 +0300

    Implimented reading Student From File

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int getRandomInt(int min, int max)
     return dist(gen);
 }
 
-//void function is empty function; does not return any value
+// void function is empty function; does not return any value
 
 void addStudentManually(vector<Student> &students)
 {
@@ -71,7 +71,7 @@ void addStudentManually(vector<Student> &students)
     cout << "Enter last name: ";
     cin >> s.lastName;
 
-    //Validate exam grade (1 - 10)
+    // Validate exam grade (1 - 10)
     cout << "Enter exam result (1-10): ";
     while (!(cin >> s.exam) || s.exam < 1 || s.exam > 10)
     {
@@ -80,7 +80,7 @@ void addStudentManually(vector<Student> &students)
         cin.ignore(numeric_limits<streamsize>::max(), '\n'); // search it
     }
 
-    //Validate number of homework assignments (0 - 10)
+    // Validate number of homework assignments (0 - 10)
     int n;
     cout << "Enter the number of homework assignments (0-10): ";
     while (!(cin >> n) || n < 0 || n > 10)
@@ -90,7 +90,7 @@ void addStudentManually(vector<Student> &students)
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    //Validate each homework score (1 - 10)
+    // Validate each homework score (1 - 10)
     for (int i = 0; i < n; i++)
     {
         int score;
@@ -108,7 +108,7 @@ void addStudentManually(vector<Student> &students)
     cout << "Student added successfully!\n";
 }
 
-//kuriame funckija kuri generuoja "automatiskai" studentus
+// kuriame funckija kuri generuoja "automatiskai" studentus
 
 void generateRandomStudents(vector<Student> &students)
 {
@@ -123,7 +123,7 @@ void generateRandomStudents(vector<Student> &students)
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    //kadangi programa negali pati sukurti, nes dudoame sarasa random vardu/pavardziu
+    // kadangi programa negali pati sukurti, nes dudoame sarasa random vardu/pavardziu
 
     const vector<string> firstNames = {
         "John", "Peter", "Anna", "Mark", "Laura",
@@ -140,7 +140,7 @@ void generateRandomStudents(vector<Student> &students)
         s.lastName = lastNames[getRandomInt(0, lastNames.size() - 1)];
 
         int homeworkCount = getRandomInt(1, 10);
-        for (int j = 0; j < homeworkCount; j++) //for loop in other for loop
+        for (int j = 0; j < homeworkCount; j++) // for loop in other for loop
         {
             s.homework.push_back(getRandomInt(1, 10));
         }
@@ -184,13 +184,17 @@ void displayStudentResults(vector<Student> &students)
     {
         cout << "Invalid choice! Enter 1, 2, or 3: ";
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); // This discards everything remaining on the line up to the Enter key, leaving the input buffer empty and ready for a fresh input attempt on the next loop iteration.
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        // This discards everything remaining on the line up to the Enter key
+        // leaving the input buffer empty and ready for a fresh input attempt on the next loop iteration.
     }
 
-    cout << fixed << setprecision(2);
+    cout << fixed << setprecision(2);   //setprecision - after the point only two decimal places
 
     if (choice == 1)
     {
+        // left, setw(n) - allying to left 
+        //used to make tables structured
         cout << left << setw(15) << "First Name" << setw(15) << "Last Name" << "Average\n";
         cout << "-------------------------------------------\n";
         for (const auto &s : students)
@@ -219,6 +223,136 @@ void displayStudentResults(vector<Student> &students)
     }
 }
 
+bool readStudentsFromFile(vector<Student> &students)
+{
+    ifstream file("kursiokai.txt"); //ifstream - read the file 
+
+    // Check if file is opened
+    if (!file.is_open())
+    {
+        cout << "Error: Could not open the file\n";
+        return false;
+    }
+
+    // Read header
+    string headerLine;
+    getline(file, headerLine);  //gives us the first line
+
+    // Count how many homework columns are in the header
+    stringstream headerStream(headerLine);
+
+    string word;
+    int homeworkCount = 0;
+
+    while (headerStream >> word)
+    {
+        // ND1, ND2, ND3, ... are homework columns
+        if (word.rfind("ND", 0) == 0) //finds the number of homework in each file
+        {
+            homeworkCount++;
+            // homeworkCount = homeworkCount + 1;
+        }
+    }
+
+    // Make sure we found homework columns
+    if (homeworkCount == 0)
+    {
+        cout << "Error: No homework columns found in the file.\n";
+        return false;
+    }
+
+    string firstName;
+    string lastName;
+
+    while (file >> firstName >> lastName) //condition to check if line exists or not
+    {
+        Student student;
+
+        student.firstName = firstName;
+        student.lastName = lastName;
+
+        //student.homework.clear(); // <-- verify if vector is empty
+
+        // Read all homework grades
+
+        for (int i = 0; i < homeworkCount; i++) 
+        {
+            int grade;
+            if (!(file >> grade))  
+            {
+                cout << "Error: Invalid homework grade for "
+                     << firstName << " " << lastName << ".\n";
+
+                return false;
+            }
+
+            // Grade must be 1-10
+            if (grade < 1 || grade > 10)
+            {
+                cout << "Error: Invalid homework grade: "
+                     << grade << "\n";
+
+                cout << "Student: "
+                     << firstName << " "
+                     << lastName << "\n";
+
+                cout << "Homework grades must be between 1 and 10.\n";
+
+                return false;
+            }
+
+            student.homework.push_back(grade);  //psuh the grade into homework vector
+        }
+
+        // Read exam grade
+        if (!(file >> student.exam)) //copying the next value from the file into student.exam
+        {
+            cout << "Error: Invalid exam grade for "
+                 << firstName << " "
+                 << lastName << ".\n";
+
+            return false;
+        }
+
+        // Validate exam grade --> condition
+        if (student.exam < 1 || student.exam > 10)
+        {
+            cout << "Error: Invalid exam grade: "
+                 << student.exam << "\n";
+
+            cout << "Student: "
+                 << firstName << " "
+                 << lastName << "\n";
+
+            cout << "Exam grade must be between 1 and 10.\n";
+
+            return false;
+        }
+
+        // Make sure there is at least one homework
+        if (student.homework.empty())
+        {
+            cout << "Error: Student has no homework grades.\n";
+            return false;
+        }
+
+        students.push_back(student);
+    }
+
+    file.close();  //uzdarome faila, nes uzsima vietos
+
+    if (students.empty())
+    {
+        cout << "Error: No students found in the file.\n";
+        return false;
+    }
+
+    cout << students.size()
+         << " students loaded successfully.\n";
+
+    return true;
+}
+
 void showMenu()
 {
     cout << "\n====================================\n";
@@ -227,7 +361,8 @@ void showMenu()
     cout << "1. Add student manually\n";
     cout << "2. Generate random students\n";
     cout << "3. Display student results\n";
-    cout << "4. Exit\n";
+    cout << "4. Read student data from file\n";
+    cout << "5. Exit\n";
 }
 
 int main()
@@ -240,15 +375,15 @@ int main()
         showMenu();
         cout << "Enter your choice (1-4): ";
 
-        //Validates main menu choice directly
-        while (!(cin >> choice) || choice < 1 || choice > 4)
+        // Validates main menu choice directly
+        while (!(cin >> choice) || choice < 1 || choice > 5)
         {
-            cout << "Invalid choice! Please enter a number from 1 to 4: ";
+            cout << "Invalid choice! Please enter a number from 1 to 5: ";
             cin.clear();
-            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
         }
 
-        switch (choice)
+        switch (choice)  //switch statement
         {
         case 1:
             addStudentManually(students);
@@ -260,11 +395,19 @@ int main()
             displayStudentResults(students);
             break;
         case 4:
+            students.clear();  //before reading from file we clear a vector
+
+            if (readStudentsFromFile(students))
+            {
+                cout << "Students loaded successfully.\n";
+            }
+            break;
+        case 5:
             cout << "Exiting program...\n";
             break;
         }
 
-    } while (choice != 4);
+    } while (choice != 5);
 
     return 0;
 }
